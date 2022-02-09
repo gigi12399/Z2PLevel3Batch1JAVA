3be3c6530724bcd33a1a857f398c30e24a646f2a
gigi12399 <snowqueen.jasmine@gmail.com>
2022-02-09 18:07:53 +0630

    Day 2 update

--- 01.Basic/src/Day2/Testing.java
+++ 01.Basic/src/Day2/Testing.java
@@ -2,9 +2,21 @@ package Day2;
 
 public class Testing {
 
+	static String name;
+	int price;
+	
+	void display() {
+		int barCode = 1001;
+		name = "Juice";
+		price = 1500;
+		System.out.println("Code: " + barCode);
+		System.out.println("Name: " + name);
+		System.out.println("Price: " + price);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Testing t1 = new Testing();
+		t1.display();
 	}
 
 }
